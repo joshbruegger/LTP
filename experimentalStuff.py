2574bc8fd9fcf7574ed8636a06b834d0e1e11e3a
Josh Bruegger <josh@bruegger.it>
2022-06-12 15:05:36 +0200

    Experiemntal

--- experimentalStuff.py
+++ experimentalStuff.py
@@ -1,36 +1,65 @@
 import re
+from shutil import ExecError
 
 import requests
 import spacy
 
+SPACY_MODEL = 'en_core_web_trf'
+SPARQL_ENDPOINT = 'https://query.wikidata.org/sparql'
 
-def main():
-    nlp = spacy.load('en_core_web_trf')
-    question = "What is a lion?"
+HEADERS = {'User-Agent': 'QASys/0.0 (https://rug.nl/LTP/; no@email.com)'}
 
-    query = '''SELECT DISTINCT ?wdLabel WHERE {
-    wd:Q144 ?wdt ?a.
-    ?wd wikibase:directClaim ?wdt .
-    SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-    }'''
+nlp = spacy.load(SPACY_MODEL)
 
+
+def query_all_properties(entity):
+    return '''SELECT ?wdLabel ?ps_Label ?wdpqLabel ?pq_Label {
+    VALUES (?entity) {(wd:$entity$)}
+    ?entity ?p ?statement .
+    ?statement ?ps ?ps_ .
+    ?wd wikibase:claim ?p.
+    ?wd wikibase:statementProperty ?ps.
+    OPTIONAL {
+    ?statement ?pq ?pq_ .
+    ?wdpq wikibase:qualifier ?pq .
+    }
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
+    } ORDER BY ?wd ?statement ?ps_'''.replace('$entity$', entity)
+
+
+def query_wikidata(query):
     try:
-        data = requests.get('https://query.wikidata.org/sparql',
-                            headers={
-                                'User-Agent': 'QASys/0.0 (https://rug.nl/LTP/; josh@bruegger.it)'},
+        return requests.get(SPARQL_ENDPOINT,
+                            headers=HEADERS,
                             params={'query': query, 'format': 'json'}).json()['results']['bindings']
-    except:
+    except Exception:
         print('Error: Query failed: Too many requests?')
 
-    print('mammal' in data)
 
-    properties = []
-    for item in data:
-        p = item['wdLabel']['value']
-        if 'ID' not in p:
-            properties.append(p)
+def main():
+    # question = "What is a lion?"
 
-    print(properties)
+    query = query_all_properties('Q670887')
+
+    data = query_wikidata(query)
+
+    print('edible' in str(data))
+
+    # print(data)
+
+    # properties = []
+    # for item in data:
+    #     p = item['wdLabel']['value']
+    #     if 'ID' not in p:
+    #         properties.append(p)
+
+    # print(properties)
 
 
 main()
+
+# query = '''SELECT DISTINCT ?wdLabel WHERE {
+# wd:Q144 ?wdt ?a.
+# ?wd wikibase:directClaim ?wdt .
+# SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+# }'''
